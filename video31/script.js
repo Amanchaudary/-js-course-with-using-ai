bae697d8063932660d2e9ee13655f7a5851bbfc7
amanecdy <amanecdy@gmail.com>
2025-03-19 09:56:09 +0900

    about funtions

---
+++ video31/script.js
@@ -0,0 +1,17 @@
+// about function 
+
+function addTwonumber(){
+    return 5+6
+}
+ let result=addTwonumber();
+ console.log(result)
+
+// function banaune tarika
+ function noRepetecode(){
+    console.log("hello")
+    console.log("my name is aman");
+    console.log("i am from chiba")
+ }
+
+//  call garne tarika
+ noRepetecode();   
