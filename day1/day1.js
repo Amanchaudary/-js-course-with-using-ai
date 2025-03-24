85d67a3cd253fee3c4fced1298ad2a33a1c465ab
amanecdy <amanecdy@gmail.com>
2025-03-24 10:16:15 +0900

    day 1 introduction setup of vscode and nodejs

--- day1/day1.js
+++ day1/day1.js
@@ -1,1 +1,13 @@
-console.log("aman")
+console.log("day1");
+//  today i learn about vs code and download node js
+// shortcut of vscode to see pdf 
+// pdf will be for  (linux/windows)     and    (macos)
+
+
+
+// link of vscode download
+// https://code.visualstudio.com/
+
+// link of nodejs download
+// https://nodejs.org/en
+
