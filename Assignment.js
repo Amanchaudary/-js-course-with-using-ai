7de5b0579b631425271b279cef12610b4169b906
amanecdy <amanecdy@gmail.com>
2025-03-28 13:20:43 +0900

    day3 Arithmetic,logical,Comparison operations will be finished

---
+++ Assignment.js
@@ -0,0 +1,27 @@
+// 2 Assignment operatos
+// tutorials
+// JavaScript Operators and Expressions | JavaScript Tutorial in Hindi #6
+
+let a=5
+a+=5// same as a=a+5  
+console.log(a);
+
+//3 Comparison operators
+console.log("comparison operations");
+let comp1=6
+let comp2="6"
+console.log("comp1==comp2 is ",comp1==comp2);
+console.log("comp1!=comp2 is ",comp1!=comp2);
+// different between == and ===
+console.log("comp1==comp2 is ",comp1===comp2);
+console.log("comp1!=comp2 is ",comp1!==comp2);
+
+
+// 4 logical operators
+// logical operators boolen form ma operate hunxa that mean asko answer true or false ma matra aauxa
+console.log("logical operators");
+let x=5
+let y=6;
+console.log(x<y&&x==5);
+console.log(x<y||x==5);
+console.log(!false);
---
+++ day3/Arithmetic.js
@@ -0,0 +1,29 @@
+// operators
+
+// tutorials
+// JavaScript Operators and Expressions | JavaScript Tutorial in Hindi #6
+console.log("Operators in javaScripts");
+
+// 1 Arithmetic Oprators
+// Arthemetic operation garna number chainxa
+// arthemetic ma increment ra decrement garna variable const ma navayar let ma banaune kinavane coste vanauda value change hudain tesaile
+console.log("Arithmetic opreators");
+let a=45;
+let b=4;
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+// power banna janxa 
+console.log("a**b=",a**b);
+// remendar value show garxa
+console.log("a%b",a%b);
+//  increment operators
+console.log("a++=",a++);
+// a++ garda suruma a print garxa ra balla value badauxa ra pheri a print garuda badheko value print garxa
+console.log("a",a);
+console.log("a++=",++a);
+
+// decrement operators
+console.log("a--=",b--);
+console.log("a--=",--b);
