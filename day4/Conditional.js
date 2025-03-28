7407ccd704789af122199d491452cb46a33e75b9
amanecdy <amanecdy@gmail.com>
2025-03-28 13:50:30 +0900

    day 4 conditional start

---
+++ day4/Conditional.js
@@ -0,0 +1,7 @@
+// Conditional Statement 
+// tutorial
+// Conditional expressions in JavaScript | JavaScript Tutorial in Hindi #7
+
+// 1 if statement
+let a=prompt("hey whats your age")
+console.log(a);
