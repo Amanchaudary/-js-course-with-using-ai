344ba19bfaf50602d0436508ce6d84c673a2dea5
amanecdy <amanecdy@gmail.com>
2025-03-30 09:14:35 +0900

    if and switch statement can learn

--- day4/Conditional.js
+++ day4/Conditional.js
@@ -1,7 +1,64 @@
-// Conditional Statement 
+// Conditional Statement
 // tutorial
 // Conditional expressions in JavaScript | JavaScript Tutorial in Hindi #7
 
 // 1 if statement
-let a=prompt("hey whats your age")
-console.log(a);
+//  (){} use garinxa ra ()vitra condition use grinxa
+let a = 6;
+if (a > 5) {
+  console.log("this is higher than 5");
+}
+
+// 2 if else statament
+// if(){}else{}  asma 2 ota codition ma auta condition matra use hunxa
+let b = 5;
+if (b > 6) {
+  console.log("this is less than 6");
+} else {
+  console.log("this is high than 6");
+}
+
+// 3 if else if statement
+// asma ni auta matra condition show hunxa
+let c = 23;
+if (c > 2) {
+    console.log("same");
+} 
+else if(c<=10){
+console.log("print");
+}
+else{
+    console.log("not print");
+}
+
+
+
+// 4 Switch statement
+// JavaScript Switch Case Tutorial in Hindi / Urdu
+//  SWitch sanga break pani use garinxa
+const day=1
+switch (day) {
+    case 0:
+        console.log("today is sunday");
+        break;
+     case 1:
+        console.log("today is monday");
+        break;   
+     case 2:
+        console.log("today is monday");
+        break;   
+     case 3:
+        console.log("today is monday");
+        break;   
+     case 4:
+        console.log("today is monday");
+        break;   
+     case 5:
+        console.log("today is monday");
+        break;   
+     case 6:
+        console.log("today is monday");
+        break;
+       default:
+        console.log("write wrong number");
+}
