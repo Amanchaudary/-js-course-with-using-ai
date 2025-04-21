85de8b7e1f41a6b6926956359211b9bb843a1f72
amanecdy <amanecdy@gmail.com>
2025-04-21 10:24:50 +0900

    no complete about  LocalStorage

---
+++ day13/localstorage.js
@@ -0,0 +1,4 @@
+// localStorage in js
+// tutorail
+// localStorage & related methods | JavaScript Tutorial in Hindi #69
+//  yo chai datebase jastai ho data save Garna use garinxa yo chai refresh garda pani date delete hudain
