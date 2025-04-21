dc38eda7db376ec9fb0d64265314298bc796709d
amanecdy <amanecdy@gmail.com>
2025-04-21 09:34:24 +0900

    Map cannot complete saw another video

---
+++ day12/maps.js
@@ -0,0 +1,7 @@
+// maps in javascript
+// tutorial
+// JavaScript Maps | JavaScript Tutorial In Hindi #57
+console.log("this is about Maps");
+//  In maps we can use any type of key or valuse and use new Map() key
+const myMap=new Map();
+console.log(myMap);
