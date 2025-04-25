5030cec59f7d28826f87922d9a124837725c22d1
amanecdy <amanecdy@gmail.com>
2025-04-25 09:33:39 +0900

    learn about localStorage

--- day13/localstorage.js
+++ day13/localstorage.js
@@ -2,3 +2,19 @@
 // tutorail
 // localStorage & related methods | JavaScript Tutorial in Hindi #69
 //  yo chai datebase jastai ho data save Garna use garinxa yo chai refresh garda pani date delete hudain
+
+
+let key=prompt("enter key you want to set")
+let value=prompt("enter value you want to set")
+// syntex
+localStorage.setItem(key,value)
+console.log(`the value at ${key} is ${localStorage.getItem(key)} `);
+
+if(key=="red"||key=="blue"){
+    localStorage.removeItem(key)
+}
+if(key==0){
+    localStorage.clear()
+}
+
+// note localstorage ko methed haru photo ma xa use garna sikne
