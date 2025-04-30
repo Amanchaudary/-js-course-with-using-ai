4deec75f33b67da47fed96b861e9f08e0c07b145
amanecdy <amanecdy@gmail.com>
2025-04-30 10:26:24 +0900

    today learn about call back

---
+++ day17/callback.js
@@ -0,0 +1,6 @@
+// callback function in js
+// Learn JavaScript CALLBACKS in 7 minutes! 🤙
+//  a callback is a funciton or a piece of code that is passed as an argument to another function
+// call back ko use chai monstly async programming ko ma garinxa
+
+
