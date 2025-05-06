f0b2d471d8a9bb8a005e2723e83ef4ac6434037d
amanecdy <amanecdy@gmail.com>
2025-05-06 23:49:10 +0900

    call back function /function ma another function parameter ko rupama use garinxa

--- day17/callback.js
+++ day17/callback.js
@@ -1,6 +1,28 @@
 // callback function in js
-// Learn JavaScript CALLBACKS in 7 minutes! 🤙
-//  a callback is a funciton or a piece of code that is passed as an argument to another function
-// call back ko use chai monstly async programming ko ma garinxa
+// Higher Order Functions and Callbacks in JavaScript | The Complete JavaScript Course | Ep.35
+// callback function vaneko auta function vitra arko function chai pareeter ko rupama pass garune kura ho
 
+// higher order function
 
+// b chai paremeter ho mouse le hover garda show garxa if gardian gane tyo argument ho
+function a(b){
+    console.log(typeof b);
+    console.log(b);
+    b()
+}
+// // string pass garda
+// a('hiii')
+// // object pass garda
+// a({username:'aman',userAge:50})
+// // array pass garda
+// a([1,2,3,4])
+
+function sayHi(){
+    console.log("hiiiiiiiiii");
+}
+// a function vitra sayHi function pass garako yo nai call back or higher order function ho
+a(sayHi)
+// another example function vitra function
+a(function(){
+    console.log("second example of call back function");
+})
