3f7919291a5b7f4616839e4e53dda1d95a7f44a9
amanecdy <amanecdy@gmail.com>
2025-05-18 10:24:22 +0900

    learn asynchronous programme

---
+++ day22/sync_vs_Async.js
@@ -0,0 +1,12 @@
+// Sync vs Async programming in js
+// tutorail ==> 2 ota video xa 2 tai herne
+//video1==> Async Javascript fundamentals | chai aur #javascript
+//video2==> 2 projects with Async JS | chai aur #javascript
+
+// video 1 topic
+// synchronous ==> 
+// java Script chai synchronous language ho 
+// yo chai mathi bata line by line code excutive hunnxa
+// single threaded language ho so ali slow hunxa aru programming ko compare ma
+
+// video 2 topic==> asko lagi sync.html file herne
