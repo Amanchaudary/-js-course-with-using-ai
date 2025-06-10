ff17b57d0c04ab46943ca1532dbe305c23d10649
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-06-10 22:33:28 +0900

    promise basic video out of 3 this is first video

--- day24/promise2.js
+++
@@ -1,3 +0,0 @@
-// tutorial
-// Introduction to Promises | JavaScript Tutorial in Hindi #54
-// 3 ota video xa herne
---
+++ day24/promise2_1.js
@@ -0,0 +1,14 @@
+// tutorial
+// Introduction to Promises | JavaScript Tutorial in Hindi #54
+// 3 ota video xa herne ==> 54,55,56video ho
+let promise=new Promise((resolve, reject) => {
+    console.log("promise");
+    resolve(36)
+})
+
+
+console.log("hello");
+setTimeout(function(){
+console.log("hello in 2 seconds");
+},2000)
+console.log("my name is john");
